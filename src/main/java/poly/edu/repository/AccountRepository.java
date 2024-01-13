29809049cfeca33cce02a05531db80c54813d572
hophuvang0111 <congviec.phuvang.ho@gmail.com>
2024-01-13 13:49:02 +0700

    shopingCart + Security

--- src/main/java/poly/edu/repository/AccountRepository.java
+++ src/main/java/poly/edu/repository/AccountRepository.java
@@ -3,7 +3,7 @@ package poly.edu.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import poly.edu.entity.Account;
-import java.util.List;
+
 
 
 public interface AccountRepository extends JpaRepository<Account, Integer>{
