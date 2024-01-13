a54336ba1624a7f0a61475d66d7ce26e2ed0f788
hophuvang0111 <congviec.phuvang.ho@gmail.com>
2024-01-13 16:10:38 +0700

    gio hang ban cuoi

--- src/main/resources/static/js/user/cart.js
+++ src/main/resources/static/js/user/cart.js
@@ -2,6 +2,7 @@ var app = angular.module("bannoithatonline", []);
 const hostListCart = "http://localhost:8080/rest/showCart";
 const hostUpQuantityProduct = "http://localhost:8080/rest/cart/up";
 const hostDownQuantityProduct = "http://localhost:8080/rest/cart/down";
+const hostDeleteProduct = "http://localhost:8080/rest/removeFromCart";
 
 app.controller("CartController", function ($scope, $http) {
   $scope.listCart = [];
@@ -28,29 +29,77 @@ app.controller("CartController", function ($scope, $http) {
     $scope.listCart.forEach((element) => {
         console.log(cart.cartId);
       if (element.cartId == cart.cartId) {
-        var urlUpQuantityProduct = `${hostUpQuantityProduct}/1/1`;
+        var urlUpQuantityProduct = `${hostUpQuantityProduct}/1/${cart.product.productid}`;
         $http.put(urlUpQuantityProduct).then((resp) => {
           $http.get(urlListCart).then((resp) => {
             $scope.listCart = resp.data;
             console.log($scope.listCart);
           });
-        });
+        })
       }
     });
   };
 
   $scope.downQuantityProduct = function (cart) {
+    var urlDeleteProduct = `${hostDeleteProduct}/${cart.cartId}`;
     $scope.listCart.forEach((element) => {
         console.log(cart.cartId);
       if (element.cartId == cart.cartId) {
-        var urlUpQuantityProduct = `${hostDownQuantityProduct}/1/1`;
+        var urlUpQuantityProduct = `${hostDownQuantityProduct}/1/${cart.product.productid}` ;
         $http.put(urlUpQuantityProduct).then((resp) => {
           $http.get(urlListCart).then((resp) => {
             $scope.listCart = resp.data;
             console.log($scope.listCart);
           });
+        }).catch((err) => {
+            Swal.fire({
+                title: 'Bạn có chắc muốn xóa không?',
+                text: 'Hành động này sẽ xóa vĩnh viễn dữ liệu!',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Xóa',
+                cancelButtonText: 'Hủy bỏ'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    $http.delete(urlDeleteProduct).then(resp => {
+                        $http.get(urlListCart).then((resp) => {
+                            $scope.listCart = resp.data;});
+                    });
+                    Swal.fire('Đã xóa!', 'Sản phẩm đã được xóa khỏi giỏ hàng.', 'Thành công');
+                } 
+            });
         });
       }
     });
   };
+
+  $scope.deleteProduct = function(cart){
+    var urlDeleteProduct = `${hostDeleteProduct}/${cart.cartId}`;
+
+    Swal.fire({
+        title: 'Bạn có chắc muốn xóa không?',
+        text: 'Hành động này sẽ xóa vĩnh viễn dữ liệu!',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Xóa',
+        cancelButtonText: 'Hủy bỏ'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $http.delete(urlDeleteProduct).then(resp => {
+                $http.get(urlListCart).then((resp) => {
+                    $scope.listCart = resp.data;});
+            });
+            Swal.fire('Đã xóa!', 'Sản phẩm đã được xóa khỏi giỏ hàng.', 'Thành công');
+        } 
+    });
+  }
+
+
+
+
+
+
+
+
+
 });
