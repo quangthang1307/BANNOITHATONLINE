1ba06d926b460f911a64afa2f3dee1fd9aadbeee
hophuvang0111 <congviec.phuvang.ho@gmail.com>
2024-01-17 17:53:06 +0700

    done

--- src/main/resources/static/js/user/checkout.js
+++ src/main/resources/static/js/user/checkout.js
@@ -8,17 +8,14 @@ app.controller("checkoutController", function ($scope,$http,$window, $timeout){
     $scope.valueDiscountCode = 0;
 
     $scope.discount = function () {
-        let foundDiscount = false;
         if ($scope.discountCode.length > 0) {
-
             $http.get(hostListDiscount).then((resp) => {
-                resp.data.forEach(element => {
+                var trust = resp.data.find(element => {
                     const currentDate = new Date();
                     const startDate = new Date(element.startDate);
                     const endDate = new Date(element.endDate);
                     if ($scope.discountCode === element.code) {
-                        if (!(currentDate >= startDate && currentDate <= endDate)) {
-                            foundDiscount = false;
+                        if (!(currentDate >= startDate && currentDate <= endDate)) {                            
                             $scope.discountCode = '';
                             Swal.fire({
                                 title: "Mã giảm giá không hợp lệ !",
@@ -30,9 +27,9 @@ app.controller("checkoutController", function ($scope,$http,$window, $timeout){
                                     title: 'custom-title-class',
                                 },
                             });
+                            return false;
                         } else if (element.quantityUsed >= element.quantity) {
-                            foundDiscount = false;
-                            $scope.discountCode = ''
+                            $scope.discountCode = '';
                             Swal.fire({
                                 title: "Mã giảm giá đã hết lượt sử dụng !",
                                 icon: "error",
@@ -43,21 +40,34 @@ app.controller("checkoutController", function ($scope,$http,$window, $timeout){
                                     title: 'custom-title-class',
                                 },
                             });
+                            return false;
                         } else if ((currentDate >= startDate && currentDate <= endDate) && (element.quantityUsed < element.quantity)) {
                             $scope.valueDiscountCode = $scope.products[0].TotalPayment * element.percent / 100;
-                            foundDiscount = true;
-                            console.log('tren', foundDiscount);
+                            return true;
                         }
                     }
+                    return false;
                 });
+
+                if (!trust) {
+                    console.log($scope.valueDiscountCode);
+                    $scope.valueDiscountCode = 0;
+                    $scope.discountCode = '';
+                    Swal.fire({
+                        title: "Mã giảm giá không hợp lệ !",
+                        icon: "error",
+                        showConfirmButton: false,
+                        timer: 3000,
+                        customClass: {
+                            popup: 'custom-popup-class',
+                            title: 'custom-title-class',
+                        },
+                    });
+                }
             })
         }
         // Dời kiểm tra xuống đây để đảm bảo kiểm tra sau khi vòng lặp kết thúc
-        if (!foundDiscount) {
-            console.log('duoi', foundDiscount);
-            console.log($scope.valueDiscountCode);
-            $scope.valueDiscountCode = 0;
-        }
+        
 
     }
 
