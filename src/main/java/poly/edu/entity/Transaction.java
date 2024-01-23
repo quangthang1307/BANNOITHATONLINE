fa2e425d39e1c34796c728b869efb7e20b460bb2
quangthang1307 <tn0429221@gmail.com>
2024-01-23 14:00:28 +0700

    CHỉnh entity Transaction

--- src/main/java/poly/edu/entity/Transaction.java
+++ src/main/java/poly/edu/entity/Transaction.java
@@ -11,6 +11,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -44,7 +45,7 @@ public class Transaction {
     @Column(name = "Bank")
     private String bank;
 
-    @ManyToOne
+    @OneToOne
     @JoinColumn(name = "OrderID")
     private Order order;
     
