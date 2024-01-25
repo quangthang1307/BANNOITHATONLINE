931a3d772397e59f7fb37fe91eedb9af34355161
DungDan <dangliendungst123@gmail.com>
2024-01-25 12:35:34 +0700

    GiamGiaSanPham

--- src/main/resources/static/js/user/product.js
+++ src/main/resources/static/js/user/product.js
@@ -11,19 +11,31 @@ const hostProductImage = "http://localhost:8080/rest/products";
 const hostDeleteAllProductInCart = "http://localhost:8080/rest/removeAllCarts";
 const hostProductSale = "http://localhost:8080/rest/product/sale";
 
+
 app.controller("productController", function ($scope, $http, $window) {
 
     $scope.listCart = [];
 
-    $scope.getCustomer = function () {
-        // Gán CustomerId người dùng
-        $http.get(hostCustomerId).then(function (response) {
-            $scope.customer = response.data;
-            $window.localStorage.setItem("customerId", JSON.stringify($scope.customer));
-        });
+    // Gán CustomerId người dùng
+    function fetchCustomer() {
+        return $http
+            .get(hostCustomerId)
+            .then(function (response) {
+                // Kiểm tra xem request có thành công không
+                if (response && response.data) {
+                    $scope.customer = response.data;
+                    $window.localStorage.setItem(
+                        "customerId",
+                        JSON.stringify($scope.customer)
+                    );
+                } else {
+                    // Xử lý khi request không thành công hoặc không có dữ liệu 
+                }
+            })
+            .catch(function (error) {
+                // console.error("Lỗi khi thực hiện request để lấy customerId:", error);       
+            });
     }
-
-
 
     //Gọi API lấy sản phẩm
     $scope.itemsPerPage = 9;
@@ -68,8 +80,6 @@ app.controller("productController", function ($scope, $http, $window) {
                 console.error('Error fetching productsale:', error);
             }
             );
-
-
     };
     //
 
@@ -93,10 +103,10 @@ app.controller("productController", function ($scope, $http, $window) {
             .then(resp => {
                 $scope.productbyid = resp.data;
                 // Trong controller hiện tại, lưu dữ liệu dạng Json và localStorage
-                if ($scope.isProductInSale(resp.data.productid)){
-                   var percent = $scope.getPercentSaleForProduct(resp.data.productid);
-                   resp.data.percent = percent;
-                }else{
+                if ($scope.isProductInSale(resp.data.productid)) {
+                    var percent = $scope.getPercentSaleForProduct(resp.data.productid);
+                    resp.data.percent = percent;
+                } else {
                     resp.data.percent = 0;
                 }
                 localStorage.setItem('productById', JSON.stringify(resp.data));
@@ -142,55 +152,7 @@ app.controller("productController", function ($scope, $http, $window) {
 
 
 
-    $scope.addToCart = function (product) {
-        var urlCheckCart =
-            "http://localhost:8080/rest/cart/" +
-            $scope.customer.customerId +
-            "/" +
-            product.productid;
-        $http
-            .get(urlCheckCart)
-            .then(function (response) {
-                if (response.data) {
-                    var url =
-                        "http://localhost:8080/rest/cart/up/" +
-                        $scope.customer.customerId +
-                        "/" +
-                        product.productid;
 
-                    $http.put(url);
-                    Swal.fire({
-                        title: "Thêm sản phẩm thành công !",
-                        text: "",
-                        icon: "success",
-                        confirmButtonText: "OK",
-                        timer: 850,
-                    });
-                } else {
-                    var url = "http://localhost:8080/rest/addToCart";
-                    var dataPost = {
-                        customer: {
-                            customerId: $scope.customer.customerId,
-                        },
-                        product: {
-                            productid: product.productid,
-                        },
-                        quantity: 1,
-                    };
-                    $http.post(url, dataPost);
-                    Swal.fire({
-                        title: "Thêm sản phẩm thành công !",
-                        text: "",
-                        icon: "success",
-                        confirmButtonText: "OK",
-                        timer: 850,
-                    });
-                }
-            })
-            .catch(function (error) {
-                console.error("Error checking cart:", error);
-            });
-    };
 
 
 
@@ -241,30 +203,105 @@ app.controller("productController", function ($scope, $http, $window) {
 
 
     }
+    //
 
 
-
-
-
-    $scope.loadCart = function () {
-        var getCustomer = localStorage.getItem("customerId");
-        var customer = JSON.parse(getCustomer);
-        console.log(customer);
-        var urlListCart = `${hostListCart}/${customer.customerId}`;
-
-        $http.get(urlListCart).then((resp) => {
-            $scope.listCart = resp.data;
-            $scope.listCart.forEach((cart) => {
-                cart.isSelected = false;
-                var urlProduct = `${hostProductImage}/${cart.product.productid}`;
-                $http.get(urlProduct).then((respProduct) => {
-                    cart.imageUrl = respProduct.data[0].image;
-                });
+    $scope.addToCart = function (product) {
+        if ($scope.customer == null) {
+            Swal.fire({
+                title: "Bạn chưa có tài khoản",
+                text: "Cần đăng nhập hoặc đăng ký",
+                icon: "question",
+                showCancelButton: true,
+                confirmButtonText: "Đăng nhập",
+                cancelButtonText: "Đăng ký",
+                timer: 10000,
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = "/login";
+                } else if (result.dismiss === Swal.DismissReason.cancel) {
+                    // Người dùng chọn "Đăng ký"
+                    console.log("Đăng ký");
+                }
             });
 
-            //Tổng tiền toàn bộ giỏ hàng
-            $scope.calculateTotalAmount();
-            //   $scope.calculateSelectedTotalAmount();
+            return;
+        }
+
+        var urlCheckCart =
+            "http://localhost:8080/rest/cart/" +
+            $scope.customer.customerId +
+            "/" +
+            product.productid;
+        $http
+            .get(urlCheckCart)
+            .then(function (response) {
+                if (response.data) {
+
+                    var url =
+                        "http://localhost:8080/rest/cart/up/" +
+                        $scope.customer.customerId +
+                        "/" +
+                        product.productid;
+
+                    $http.put(url, null, { params: { quantity: response.data.quantity + 1 } });
+                    $scope.loadCart();
+                    Swal.fire({
+                        title: "Thêm sản phẩm thành công !",
+                        text: "",
+                        icon: "success",
+                        confirmButtonText: "OK",
+                        timer: 850,
+                    });
+                }
+            })
+            .catch(function (error) {
+                var url = "http://localhost:8080/rest/addToCart";
+                var dataPost = {
+                    customer: {
+                        customerId: $scope.customer.customerId,
+                    },
+                    product: {
+                        productid: product.productid,
+                    },
+                    quantity: 1,
+                };
+                $http.post(url, dataPost);
+                $scope.loadCart();
+                Swal.fire({
+                    title: "Thêm sản phẩm thành công !",
+                    text: "",
+                    icon: "success",
+                    confirmButtonText: "OK",
+                    timer: 850,
+                });
+            });
+    };
+
+    $scope.loadCart = function () {
+        // Use the fetchCustomer function to ensure it completes before moving to the next step
+        fetchCustomer().then(function () {
+            if (window.localStorage.getItem("customerId")) {
+                var getCustomer = localStorage.getItem("customerId");
+                var customer = JSON.parse(getCustomer);
+                console.log(customer);
+                var urlListCart = `${hostListCart}/${customer.customerId}`;
+
+                $http.get(urlListCart).then((resp) => {
+                    $scope.listCart = resp.data;
+                    $scope.listCart.forEach((cart) => {
+                        cart.isSelected = false;
+                        var urlProduct = `${hostProductImage}/${cart.product.productid}`;
+                        $http.get(urlProduct).then((respProduct) => {
+                            cart.imageUrl = respProduct.data[0].image;
+                        });
+                    });
+
+                    //Tổng tiền toàn bộ giỏ hàng
+                    $scope.calculateTotalAmount();  
+                    //   $scope.calculateSelectedTotalAmount();
+                });
+            }
         });
     };
 
@@ -272,19 +309,28 @@ app.controller("productController", function ($scope, $http, $window) {
         $scope.totalAmount = 0;
 
         $scope.listCart.forEach(function (cartItem) {
-            var productPrice = cartItem.product.pricexuat;
+
+            var percent = 0;
+            console.log("san pham khong sale:"+$scope.isProductInSale(cartItem.product.productid));
+            console.log(cartItem.product.productid);
+            if($scope.isProductInSale(cartItem.product.productid)){
+                percent = $scope.getPercentSaleForProduct(cartItem.product.productid);
+            }
+            console.log(percent);
+            var productPrice = cartItem.product.pricexuat - (cartItem.product.pricexuat * percent/100);
             var quantity = cartItem.quantity;
             $scope.totalAmount += productPrice * quantity;
+            cartItem.product.pricexuat = productPrice;
         });
+
     };
 
-    // Call loadCart when the customer is loaded
-    $scope.getCustomer();
-    $scope.loadCart();
     // Load data for the first time
     $scope.getData();
     $scope.getDataCategory();
 
+    // Call loadCart when the customer is loaded
+    $scope.loadCart();
 
 
 
@@ -296,5 +342,5 @@ app.controller('productDetailController', function ($scope, $http) {
     console.log("ProductDetailController");
     $scope.productdetail = JSON.parse(localStorage.getItem('productById'));
 
-    console.log("ProductDetail", $scope.productdetail); 
+    console.log("ProductDetail", $scope.productdetail);
 });
