6f08c149a6e02e1081cf6ec61b46e9099d563527
quangthang1307 <tn0429221@gmail.com>
2024-03-07 13:27:22 +0700

    Thêm biểu đồ tròn admin

--- src/main/java/poly/edu/repository/OrderRepository.java
+++ src/main/java/poly/edu/repository/OrderRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import poly.edu.entity.Order;
 import poly.edu.entity.Orderstatus;
@@ -16,37 +17,15 @@ public interface OrderRepository extends JpaRepository<Order, Integer> {
     @Query(value = "select * from [Order] where CustomerID = ?1 and OrderID = ?2", nativeQuery = true)
     Order getOrderByCustomerIdAndOrderId(Integer customerId, Integer orderId);
 
-    // Order findBySumpayment(Double sumpayment);
-
-    // @Query(value = "SELECT SUM(o.Sumpayment) FROM [Order] o JOIN Orderstatus os
-    // ON o.OrderstatusID = os.OrderstatusID WHERE os.Orderstatusname = ?1 and
-    // MONTH(o.[Time]) = ?2 AND YEAR(o.[Time]) = ?3 and o.PaymentID = ?4",
-    // nativeQuery = true)
-    // Integer findSumpaymentOrder(String payment, Integer month, Integer year,
-    // Integer paymentid);
-
-    // @Query(value = "SELECT SUM(o.[Sumpayment])FROM [Order] o JOIN [Orderstatus]
-    // os ON o.[OrderstatusID] = os.[OrderstatusID] JOIN [Payment] p ON
-    // o.[PaymentID] = p.[PaymentID] WHERE os.[Orderstatusname] = ?1 AND
-    // p.[Paymentname] = ?2 AND MONTH(o.[Time]) = ?3 AND YEAR(o.[Time]) = ?4",
-    // nativeQuery = true)
-    // Integer findSumpaymentOrder(String payment, String paymentname, Integer
-    // month, Integer year);
-
-    // @Query(value = "SELECT SUM(o.Sumpayment) FROM [Order] o JOIN Payment p ON
-    // o.PaymentID = p.PaymentID JOIN Transactions t ON o.OrderID = t.OrderID WHERE
-    // p.Paymentname = ?1 AND t.[Status] = 0 AND MONTH(o.[Time]) = ?2 AND
-    // YEAR(o.[Time]) = ?3", nativeQuery = true)
-    // Integer findTransactionsOrder(String paymentName, Integer month, Integer
-    // year);
-
     @Query(value = "SELECT SUM(o.[Sumpayment])FROM [Order] o JOIN [Orderstatus] os ON o.[OrderstatusID] = os.[OrderstatusID] WHERE os.[Orderstatusname] = ?1 AND MONTH(o.[Time]) = ?2 AND YEAR(o.[Time]) = ?3", nativeQuery = true)
     Integer findSumpaymentOrder(String orderStatus, Integer month, Integer year);
 
     @Query(value = "SELECT DISTINCT YEAR(o.[Time]) FROM [Order] o ORDER BY YEAR(o.[Time]) DESC", nativeQuery= true)
     List<Integer> findDistinctYears();
 
-
+    @Query(value = "SELECT cp.Categoryname, COUNT(od.ProductID) AS SoLuong FROM Categoryproduct cp JOIN Product p ON cp.ID = p.CategoryID JOIN OrderDetails od ON p.ProductID = od.ProductID JOIN [Order] o ON od.OrderID = o.OrderID JOIN Orderstatus os ON o.OrderstatusID = os.OrderstatusID WHERE os.Orderstatusname = N'Thanh toán' AND YEAR(o.[Time]) = :selectedYear GROUP BY cp.Categoryname", nativeQuery = true)
+    List<Object[]> countProductByCategoryAndStatus(@Param("selectedYear") Integer selectedYear);
+    
     @Query(value = "Select * from [Order] where OrderstatusID = ?1 and CustomerID = ?2", nativeQuery = true)
     List<Order> findByOrderstatus_OrderstatusID_CustomerID(Integer orderstatusId, Integer customerId);
 
