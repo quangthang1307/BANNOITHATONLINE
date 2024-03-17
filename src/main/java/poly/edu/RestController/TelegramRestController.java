6171c4e98d2be4c39ab52d9a2c3646e89a3d3a6d
hophuvang0111 <congviec.phuvang.ho@gmail.com>
2024-03-17 13:22:09 +0700

    Done LINK hyper

--- src/main/java/poly/edu/RestController/TelegramRestController.java
+++ src/main/java/poly/edu/RestController/TelegramRestController.java
@@ -7,19 +7,25 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.request.ParseMode;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 
+import jakarta.servlet.http.HttpServletRequest;
 import poly.edu.entity.Order;
 import poly.edu.entity.Orderdetails;
+import poly.edu.entity.Orderstatus;
 import poly.edu.repository.OrderDetailRepository;
 import poly.edu.repository.OrderRepository;
+import poly.edu.repository.OrderStatusRepository;
 
 @RestController
 @CrossOrigin("*")
@@ -29,6 +35,23 @@ public class TelegramRestController {
     OrderDetailRepository orderDetailRepository;
     @Autowired
     OrderRepository orderRepository;
+    @Autowired OrderStatusRepository orderStatusRepository;
+
+    @Autowired
+    HttpServletRequest request;
+
+    @GetMapping("/rest/telegram/approve")
+    public boolean approve(@RequestParam Integer orderID){
+        Optional<Order> order = orderRepository.findById(orderID);
+        if(order.isPresent()){
+            Orderstatus orderstatus = orderStatusRepository.findByOrderStatusName("Đã xác nhận");
+            order.get().setOrderstatus(orderstatus);;
+            orderRepository.save(order.get());
+            return true;
+        } else{
+            return false;
+        }
+    }
 
     @PostMapping("/rest/telegram/notification")
     public boolean notification(@RequestBody Map<String, Object> data) throws JsonProcessingException {
@@ -38,6 +61,7 @@ public class TelegramRestController {
         String tongtien = null;
         String time = null;
         String name = null;
+        String link = null;
         int orderID = (int) data.get("data");
         Optional<Order> order = orderRepository.findById(orderID);
         List<Orderdetails> orderDetail = orderDetailRepository.getOrderdetailsByOrderID(orderID);
@@ -69,18 +93,26 @@ public class TelegramRestController {
             if(order.get().getCustomer().getName() != null || !order.get().getCustomer().getName().isEmpty()){
                 name = order.get().getCustomer().getName();
             }
+            
                 DecimalFormat decimalFormat = new DecimalFormat("#,### VND");                
                 tongtien = decimalFormat.format(Double.parseDouble(order.get().getSumpayment().toString()));
+                String baseUrl = request.getRequestURL().toString().replace(request.getRequestURI(), request.getContextPath());
+                link = baseUrl + "/rest/telegram/approve?orderID=" + String.valueOf(orderID);
         }
 
-         
+        String message = "Đơn hàng mới!\n"
+        + "*Họ và tên:* " + name + "\n"
+        + "*Username:* " + username + "\n"
+        + "*Số điện thoại:* " + phone + "\n"
+        + "*Ngày đặt hàng:* " + time + "\n"
+        + "*Sản phẩm:* " + sanpham + "\n"
+        + "*Tổng tiền:* " + tongtien + "\n"
+        + "Link duyệt đơn hàng: " + "[Ấn vào đây](http://www.localhost/rest/telegram/approve?orderID="+ String.valueOf(orderID) +")";
 
 
         try {
             TelegramBot bot = new TelegramBot("6853009990:AAHzyQYieOgmEUBr6cAI8-OXGQD_t_GHVW4");
-            SendMessage send = new SendMessage("5884779776", String.valueOf("Đơn hàng mới!\n" + "Họ và tên: " + name + "\n" + 
-            "Username: " + username + "\n" + "Số điện thoại: " + phone + "\n" + "Ngày đặt hàng: " + time + "\n" + "Sản phẩm: " + sanpham + "\n" + 
-            "Tổng tiền: " + tongtien));
+            SendMessage send = new SendMessage("5884779776", message).parseMode(ParseMode.Markdown);
             SendResponse response = bot.execute(send);
             return true;
         } catch (Exception e) {
