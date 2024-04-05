1acde6fdaf70bfa2bb509507f99b28bd3e666bc2
hophuvang0111 <congviec.phuvang.ho@gmail.com>
2024-04-05 13:18:57 +0700

    Inventory

--- src/main/java/poly/edu/repository/InventoryRepository.java
+++ src/main/java/poly/edu/repository/InventoryRepository.java
@@ -3,7 +3,14 @@ package poly.edu.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import poly.edu.entity.Inventory;
+import poly.edu.entity.Product;
+
 
 @Repository
 public interface InventoryRepository
-  extends JpaRepository<Inventory, Integer> {}
+  extends JpaRepository<Inventory, Integer> {
+
+    Inventory findByProduct(Product product);
+
+
+  }
