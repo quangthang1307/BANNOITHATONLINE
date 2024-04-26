3a9dde2e6b57b84434a6ca87c91c096a668021d0
hophuvang0111 <congviec.phuvang.ho@gmail.com>
2024-04-26 15:40:59 +0700

    Scheduled

--- src/main/java/poly/edu/BannoithatonlineApplication.java
+++ src/main/java/poly/edu/BannoithatonlineApplication.java
@@ -2,8 +2,11 @@ package poly.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class BannoithatonlineApplication {
 
 	public static void main(String[] args) {
@@ -12,6 +15,10 @@ public class BannoithatonlineApplication {
 		
 	}
 
-	
+	@Scheduled(cron = "0 32 15 * * *", zone = "GMT+7")
+    public void runTaskAt12GMT7() {
+        // Thực hiện đoạn code bạn muốn chạy vào 12h GMT+7 ở đây
+        System.out.println("Đã chạy đoạn code vào 12h GMT+7.");
+    }
 
 }
