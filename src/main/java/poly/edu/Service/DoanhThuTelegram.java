7b3c935d04d0c1442a35fb220739aeb341256e87
hophuvang0111 <congviec.phuvang.ho@gmail.com>
2024-05-02 08:31:56 +0700

    UPDATE BOT TELEGRAM THONG KE DOANH THU

--- src/main/java/poly/edu/Service/DoanhThuTelegram.java
+++ src/main/java/poly/edu/Service/DoanhThuTelegram.java
@@ -6,9 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import com.pengrad.telegrambot.TelegramBot;
@@ -20,6 +17,7 @@ import poly.edu.entity.Orderdetails;
 import poly.edu.entity.Telegram;
 import poly.edu.repository.OrderDetailRepository;
 import poly.edu.repository.OrderRepository;
+import poly.edu.repository.ProductRepository;
 import poly.edu.repository.TelegramRepository;
 
 @Service
@@ -31,6 +29,8 @@ public class DoanhThuTelegram {
     @Autowired
     TelegramRepository telegramRepository;
 
+    @Autowired ProductRepository productRepository;
+
     public void doanhThu() {
         Double doanhThuDatHang = 0.0;
         Double doanhThuThanhCong = 0.0;
@@ -38,7 +38,7 @@ public class DoanhThuTelegram {
         List<Order> order = orderRepository.findAll();
         List<Order> ordernow = new ArrayList<>();
 
-        if(order.isEmpty()) {
+        if (order.isEmpty()) {
             // Thực hiện các hành động tương ứng khi danh sách order trống
             System.out.println("Không có đơn hàng nào.");
             return; // Trả về ngay sau khi kiểm tra danh sách order trống
@@ -52,6 +52,7 @@ public class DoanhThuTelegram {
 
         for (Order order2 : ordernow) {
             doanhThuDatHang += order2.getSumpayment();
+
         }
 
         for (Order order2 : ordernow) {
@@ -59,13 +60,13 @@ public class DoanhThuTelegram {
             for (Orderdetails orderdetail : orderdetails) {
                 if (orderdetail.getOrder().getOrderstatus().getOrderstatusname().equals("Thanh toán thành công")) {
                     doanhThuThanhCong += orderdetail.getTotalpayment();
-                }
+                }               
             }
         }
 
         DecimalFormat df = new DecimalFormat("#,##0");
 
-        String message = "Doanh thu hôm nay - " + LocalDate.now() + "\n"
+        String message = "Tổng kết hôm nay - " + LocalDate.now() + "\n"
                 + "Doanh thu các đơn đặt hàng: " + df.format(doanhThuDatHang) + "\n"
                 + "Doanh thu các đơn thành công: " + df.format(doanhThuThanhCong) + "\n";
         Telegram telegram = telegramRepository.findByMission(Telegram.MissionType.THONGKE);
